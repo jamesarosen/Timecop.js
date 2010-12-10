5a8b595861f35f06516b3012625d5033dd3a6173
James A. Rosen <james@zendesk.com>
2010-12-10 14:36:32 -0800

    added Rakefile with :build task
    :lint and :test tasks don't do anything yet

---
+++ Rakefile
@@ -0,0 +1,42 @@
+# Build-related information:
+
+TIMECOP_VERSION = '0.0.1'
+output_path = File.expand_path("../timecop-#{TIMECOP_VERSION}.js", __FILE__)
+lib_dir = File.expand_path('../lib', __FILE__)
+lib_files = [ 'Timecop', 'MockDate', 'TimeStackItem' ].map do |file|
+  File.join(lib_dir, "#{file}.js")
+end
+
+# Tasks:
+
+task :default => :build
+
+desc "compile, syntax-check, and test the Timecop library"
+task :build => :test
+
+desc "Run tests on the compiled Timecop library"
+task :test => :lint do
+  puts "No command-line tests yet."
+end
+
+desc "Run JSLint syntax checks on the compiled Timecop library"
+task :lint => output_path do
+  puts "No JSLint yet."
+end
+
+desc "compile the files in lib/ to timecop-{version}.js"
+file output_path => lib_files do
+  template = File.read(File.join(lib_dir, 'BuildTemplate.js'))
+
+  contents = lib_files.
+              map { |f| File.read(f) }.
+              join("\n\n")
+
+  compiled = template.
+              sub('{{ TIMECOP_VERSION }}', TIMECOP_VERSION).
+              sub('{{ TIMECOP_LIBRARIES }}', contents)
+
+  File.open(output_path, 'w') { |f| f.write(compiled) }
+
+  puts("Wrote #{output_path}");
+end
--- timecop-0.0.1.js
+++ timecop-0.0.1.js
@@ -1,3 +1,16 @@
+//  Timecop.js version 0.0.1
+//  (c) 2010 James A. Rosen, Zendesk Inc.
+//  Timecop.js is freely distributable under the MIT license.
+//  The concept and some of the structure is borrowed from Timecop,
+//  John Trupiano's Ruby gem, which can be found at
+//  https://github.com/jtrupiano/timecop. Some project structure
+//  inspired by Underscore, Jeremy Ashkenas's Javascript library for
+//  functional programming support, which can be found at
+//  https://github.com/documentcloud/underscore.
+//  For all details, documentation, and bug reports, see
+//  http://github.com/jamesarosen/Timecop.js
+
+(function(){
 // Establish the root object, `window` in the browser, or `global` on the server.
 var root = this;
 
@@ -124,3 +137,73 @@ root.Timecop = Timecop;
 
 // Export it as a V8 module, if applicable:
 if (typeof exports !== 'undefined') exports._ = _;
+
+
+// A mock Date implementation.
+Timecop.MockDate = function() {
+  if (arguments.length > 0 || !Timecop.topOfStack()) {
+    this._underlyingDate = Timecop.buildNativeDate.apply(Timecop, Array.prototype.slice.apply(arguments));
+  } else {
+    this._underlyingDate = Timecop.topOfStack().date();
+  }
+};
+
+Timecop.MockDate.prototype = {
+  getDate:            function() { return this._underlyingDate.getDate(); },
+  getDay:             function() { return this._underlyingDate.getDay(); },
+  getFullYear:        function() { return this._underlyingDate.getFullYear(); },
+  getHours:           function() { return this._underlyingDate.getHours(); },
+  getMilliseconds:    function() { return this._underlyingDate.getMilliseconds(); },
+  getMinutes:         function() { return this._underlyingDate.getMinutes(); },
+  getMonth:           function() { return this._underlyingDate.getMonth(); },
+  getSeconds:         function() { return this._underlyingDate.getSeconds(); },
+  getTime:            function() { return this._underlyingDate.getTime(); },
+  getTimezoneOffset:  function() { return this._underlyingDate.getTimezoneOffset(); },
+  getUTCDate:         function() { return this._underlyingDate.getUTCDate(); },
+  getUTCDay:          function() { return this._underlyingDate.getUTCDay(); },
+  getUTCFullYear:     function() { return this._underlyingDate.getUTCFullYear(); },
+  getUTCHours:        function() { return this._underlyingDate.getUTCHours(); },
+  getUTCMilliseconds: function() { return this._underlyingDate.getUTCMilliseconds(); },
+  getUTCMinutes:      function() { return this._underlyingDate.getUTCMinutes(); },
+  getUTCMonth:        function() { return this._underlyingDate.getUTCMonth(); },
+  getUTCSeconds:      function() { return this._underlyingDate.getUTCSeconds(); },
+  getYear:            function() { return this._underlyingDate.getYear(); },
+  toString:           function() { return this._underlyingDate.toString(); }
+};
+
+
+// A data class for carrying around 'time movement' objects.
+// Makes it easy to keep track of the time movements on a simple stack.
+Timecop.TimeStackItem = function(mockType, time) {
+  if (mockType !== 'freeze' && mockType !== 'travel') {
+    throw 'Unknown mock_type ' + mockType;
+  }
+  this.mockType = mockType;
+  this._time = time;
+  this._travelOffset = this._computeTravelOffset();
+};
+
+Timecop.TimeStackItem.prototype = {
+  date: function() {
+    return this.time();
+  },
+
+  time: function() {
+    if (this._travelOffset === null) {
+      return this._time;
+    }
+    // console.log('Now: ');
+    return new Timecop.NativeDate((new Timecop.NativeDate()).getTime() + this._travelOffset);
+  },
+
+  // @api private
+  // @return [Integer] millisecond offset traveled, if mockType is 'travel'
+  _computeTravelOffset: function() {
+    if (this.mockType === 'freeze') {
+      return null;
+    }
+    return this._time.getTime() - (new Timecop.NativeDate()).getTime();
+  }
+};
+
+)();
